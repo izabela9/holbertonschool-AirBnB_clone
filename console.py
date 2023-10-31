8d3cd3071f04afa7b0124289b6881ba7a5450091
Ergi Lama <ergilama98@gmail.com>
2023-10-31 05:00:04 -0700

    Console

--- console.py
+++ console.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+import cmd
+
+class cmd.CMD:
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        An empty line + ENTER shouldn’t execute anything
+        """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
