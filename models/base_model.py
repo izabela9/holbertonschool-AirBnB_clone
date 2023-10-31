bd03caf43cccd6809a68ce36e1f72d66b831514b
izabela9 <isabelanelaj1@gmail.com>
2023-10-31 12:26:38 +0100

    base model

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Module for base model
+'''
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''
+    Class for base objects
+    '''
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
