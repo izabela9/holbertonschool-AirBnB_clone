ad6e3fb9a9599fbcd7925828a714023f7dfcb473
izabela9 <isabelanelaj1@gmail.com>
2023-11-01 13:56:31 +0100

    file storage reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,7 +23,7 @@ class FileStorage:
         '''
         function to set obj in objects
         '''
-        key = f"{self.__class__.__name__}.{obj.id}"
+        key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
