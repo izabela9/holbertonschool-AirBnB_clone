69df0b7a6aa6ad81b046c43b4dfdb3ce979f6729
Ergi Lama <ergilama98@gmail.com>
2023-11-02 02:26:40 -0700

    Cosnole 7

--- console.py
+++ console.py
@@ -11,6 +11,57 @@ class HBNBCommand(cmd.Cmd):
     Class HBNB
     """
     prompt = '(hbnb) '
+    
+
+    def __init__(self):
+        self.commands = {
+                "create": self.create,
+                "show": self.show,
+                "destroy": self.destroy,
+                "all": self.all,
+                "update": self.update
+                }
+
+    def create(self, args):
+        """
+        Create method
+        """
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        else:
+            instance = BaseModel()
+            instance = save()
+            print(instance.id)
+
+    def show(self, args):
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            instance_id = args[1]
+            if instance_id not in storage.all(BaseModel):
+                print("** no instance found **")
+            else:
+                instance = storage.all(BaseModel)[instamnce_id]
+                print(str(instance))
+
+    def destroy(self, args):
+
+
+    def all(self, args):
+
+    def update(self, args):
+
+    def run(self, args):
+        while True:
+            command = input("$ ").split()
+            if command[0] in self.commands:
+                self.commands[comand[0]](command[1:])
 
     def do_quit(self, arg):
         """
